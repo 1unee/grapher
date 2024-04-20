8d672b0b43b43be5018fe846c3e1056fbf523369
GeraAone <126874598+1unee@users.noreply.github.com>
2024-04-20 15:25:03 +0300

    add config

--- rest-grapher/src/main/java/com/oneune/grapher/service/parser/RedFeatureCollectionParsingService.java
+++ rest-grapher/src/main/java/com/oneune/grapher/service/parser/RedFeatureCollectionParsingService.java
@@ -1,7 +1,14 @@
 package com.oneune.grapher.service.parser;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oneune.grapher.store.dto.red.RedFeatureCollectionDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
 @Service
+@RequiredArgsConstructor
 public class RedFeatureCollectionParsingService {
+
+    private final ObjectMapper objectMapper;
 }
